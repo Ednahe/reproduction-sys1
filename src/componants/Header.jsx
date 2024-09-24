9e5ad87e72cdac9ea675a4fb9cc0ca19d36bf251
Manu <emmanuel.donahue@hotmail.fr>
2024-09-24 18:57:43 +0200

    suite

--- src/componants/Header.jsx
+++ src/componants/Header.jsx
@@ -8,14 +8,14 @@ const Header = () => {
 
     return <header>
         <div className="top-head">
-            <div>
+            <div className="contain-head">
                 <a href="https://sys1.fr/#contact_bloc" target="_blank">
-                <img src={devis} className="img-head" /></a>
+                <img src={devis} className="img-head" />Demander un devis</a>
             </div>
-            <div>
+            <div className="contain-head right-head">
                 <a href="https://support.sys1.fr/" target="_blank">
-                <img src={support} className="img-head" /></a>
-                <a href="#"></a>
+                <img src={support} className="img-head" />Déclarer ou suivre un incident</a>
+                <a href="#" className="intervention">Intervention à distance</a>
             </div>
         </div>
         <Nav />
