c9bed1118677c8bf3a92e2428029f08950168ae0
Manu <emmanuel.donahue@hotmail.fr>
2024-09-26 14:31:18 +0200

    suite

--- src/componants/Nav.jsx
+++ src/componants/Nav.jsx
@@ -1,10 +1,43 @@
 import React from "react";
+import logo from '../images/logo.png';
+import '../styles/nav.css';
 
 const Nav = () => {
 
-    return <nav>
-
-    </nav>
+    return <div className="contain-nav">
+        <div className="contain-logo">
+            <a href="https://sys1.fr/">
+            <img src={logo} className="logo"/></a>
+        </div>
+        <nav>
+            <ul>
+                <li className="first-li">
+                    <a href="#" className="a-nav">Votre société</a>
+                </li>
+                <li>
+                    <a href="#" className="a-nav">Vos services</a>
+                </li>
+                <li>
+                    <a href="#" className="a-nav">Vos solutions</a>
+                </li>
+                <li>
+                    <a href="#" className="a-nav">Vos logiciels</a>
+                </li>
+                <li>
+                    <a href="#" className="a-nav">Votre support</a>
+                </li>
+                <li>
+                    <a href="#" className="a-nav">Vous rejoindre</a>
+                </li>
+                <li>
+                    <a href="#" className="a-nav">Vos actualités</a>
+                </li>
+                <li>
+                    <a href="#" className="a-nav">Contact</a>
+                </li>
+            </ul>
+        </nav>
+    </div>
 };
 
 export default Nav;
